6037b2117dfe32ff3697968cdd8796d9e190d477
Devraj Mehta <devm33@github.com>
2023-07-21 14:47:08 -0400

    Add visibility into test

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const {all} = require('./index.js');
 const pems = all();
 
 if (process.platform === "win32") {
+  console.log(`Found ${pems.length} certificates`);
   assert(pems.length > 0);
 } else {
   assert(pems.length === 0);
