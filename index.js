71c699cb845ec8b4f1a0e99175368ded9ef90b3c
Devraj Mehta <devm33@github.com>
2023-07-20 23:00:07 -0400

    Switch from node-forge to crypto

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function all() {
   const crypt = require("./crypt32.node");
-  const forge = require("node-forge");
-  const ders = [];
+  const {X509Certificate} = require('crypto');
+  const pems = [];
   const store = new crypt.Crypt32();
   try {
     let der;
     while ((der = store.next())) {
-      ders.push(der);
+      const cert = new X509Certificate(der);
+      pems.push(cert.toString());
     }
   } finally {
     store.done();
   }
-  return Array.from(new Set(ders.map(forge.pki.certificateToPem)));
+  return Array.from(new Set(pems));
 }
 
 if (process.platform !== "win32") {
